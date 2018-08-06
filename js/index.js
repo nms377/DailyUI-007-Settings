b438fc999613eda4cae6838d10ee86babc9259b9
Nicole Sandry <nms377@nyu.edu>
2018-08-06 13:15:21 -1000

    Fixed Inputs Three and Four. Used a for loop to iterate through the various elements with class settingLabel so they all change their font style and weight when the slider is checked. Updated the fourth input by changing the font Family to Cookie and increasing the font size so it is a similar size compared to the Times New Roman font.

--- js/index.js
+++ js/index.js
@@ -1,44 +1,54 @@
 function settings() {
-	var bgColor = 'white';
+	var bgColor = "white";
 	document.body.style.backgroundColor = bgColor;
-	var input1 = document.getElementById('inputOne');
-	var input2 = document.getElementById('inputTwo');
-	var input3 = document.getElementById('inputThree');
-	var input4 = document.getElementById('inputFour');
+	var input1 = document.getElementById("inputOne");
+	var input2 = document.getElementById("inputTwo");
+	var input3 = document.getElementById("inputThree");
+	var input4 = document.getElementById("inputFour");
+	var labels = document.getElementsByClassName("settingLabel");
 
-	input1.addEventListener('click', function() {
+	input1.addEventListener("click", function() {
 		if (input1.checked) {
-			document.body.style.backgroundColor ='pink';
+			document.body.style.background = "powderblue";
 		} else {
-			document.body.style.backgroundColor = bgColor;
+			document.body.style.background = bgColor;
 		}
 	});
 
-	input2.addEventListener('click', function() {
+	input2.addEventListener("click", function() {
 		if (input2.checked) {
-			document.getElementById('settings').style.border = '2px dashed purple';
+			document.getElementById("settings").style.border = "none";
 		} else {
-			document.getElementById('settings').style.border = '2px solid black';
+			document.getElementById("settings").style.border = "2px solid black";
 		}
 	});
 
-	input3.addEventListener('click', function() {
-		if (input3.checked) {
-			document.getElementsByClassName('settingLabel').style.fontWeight = 'bold';
-		} else {
-			document.getElementsByClassName('settingLabel').style.fontWeight = 'normal';
+	input3.addEventListener("click", function() {
+
+		for (var i=0; i < labels.length; i++) {
+			if (input3.checked) {
+				document.getElementsByClassName("settingLabel")[i].style.fontFamily = "arial, sans-serif";
+				document.getElementsByClassName("settingLabel")[i].style.fontWeight = "bold";
+			} else {
+				document.getElementsByClassName("settingLabel")[i].style.fontFamily = "Times New Roman, serif";
+				document.getElementsByClassName("settingLabel")[i].style.fontWeight =
+					"normal";
+			}
 		}
+
 	});
 
-	input4.addEventListener('click', function() {
+	input4.addEventListener("click", function() {
 		if (input4.checked) {
-			document.getElementsByTagName('H1').style.color = 'powderblue';
+			document.getElementById("header").style.fontSize = "3rem";
+			document.getElementById("header").style.fontFamily = "Cookie, serif";
+			document.getElementById("header").style.textTransform = "capitalize";
 		} else {
-			document.body.style.backgroundColor = bgColor;
+			document.getElementById("header").style.fontSize = "2rem";
+			document.getElementById("header").style.fontFamily = "serif";
+			document.getElementById("header").style.textTransform = "uppercase";
 		}
 	});
-
-
 }
 
 settings();
